027c6a1a03e17e6a3ed0de2df5b2a358545a7238
Matt Corallo <git@bluematt.me>
2026-04-07 01:19:01 +0000

    Add missing `OffersMessageHandler::best_block` updating
    It seems we forgot to ensure `OffersMessageHandler::best_block` is
    consistently updated, leading to us building invalid blinded
    payment paths for short-lived payment paths after two weeks without
    restart.

--- lightning/src/offers/flow.rs
+++ lightning/src/offers/flow.rs
@@ -210,9 +210,11 @@ where
 	///
 	/// Must be called whenever a new chain tip becomes available. May be skipped
 	/// for intermediary blocks.
-	pub fn best_block_updated(&self, header: &Header, _height: u32) {
+	pub fn best_block_updated(&self, header: &Header, height: u32) {
 		let timestamp = &self.highest_seen_timestamp;
 		let block_time = header.time as usize;
+
+		*self.best_block.write().unwrap() = BestBlock::new(header.block_hash(), height);
 
 		loop {
 			// Update timestamp to be the max of its current value and the block
